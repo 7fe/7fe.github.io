99afd353aa73ef4a1bcb2a5ff86320b6572bdad6
Ferris <limeblack7@gmail.com>
2021-05-31 10:27:57 -0400

    Update wolfram.js

--- wolfram.js
+++ wolfram.js
@@ -1,3 +1,57 @@
 (function(){
   alert('wolfram');
 })();
+/*
+document.write(math.simplify('x^2*x').toString());
+document.write(math.simplify('x^2*x').toString());
+
+//failure doesn't scroll properly 
+var i = document.createElement('iframe');
+i.style.width = '100%';
+i.style.height = '1000px';
+i.style.position = 'absolute';
+i.style.top = 0;
+i.style.left = 0;
+i.href = "https://www.wolframalpha.com/";
+document.documentElement.appendChild(i);
+document.body.style.overflow = 'hidden';
+
+//JS bookmarklet is next idea(an extension should be makable)
+
+javascript:
+
+if (evt.keyCode == 13 && evt.shiftKey) {
+  // textarea multiline
+}
+
+javascript:(function(){alert(1);})();
+
+mathjs.org
+
+$`Banana + Apple`
+$`for(var i=0;i<10;i++){console.log(i)}`
+
+    print(math.simplify('c+o^2 - o^2'));
+
+C4H9_COO_C2H5
+
+bananana  C_7+H_14+O_2
+ltheanine C_7+H_14+N_2+O_3
+
+ltheanine = banana + n^2 + O
+
+c19h28o2
+
+c19h28o2
+tesosterone`mathematica dvorak`
+
+benperidol most potent antipsychotic
+
+chemistry_eq(name       ,abbreviation           ,formula    ,formulaexpanded   ,madeup             ,reaction);
+chemistry_eq('ltheanine','chno71423','C7H14N2O3','C^7+H^14+N^2+O^3','banana * n^2 * o','');
+
+
+//chno714
+
+Python Floats, Integers complex
+*/
